d1336bc6e32e3d3dde83a810a716f197c72b7c65
RdMaxes <yc.huang0522@gmail.com>
2014-07-25 17:57:51 +0800

    rewrite commnet

--- User_Lib/uIP-UserApp/tcp_demo.h
+++ User_Lib/uIP-UserApp/tcp_demo.h
@@ -6,16 +6,17 @@
 #include "uipopt.h"
 #include "psock.h"
 
-//通信程序状态字(用户可以自己定义)  
+//comminication status
 enum
 {
-	STATE_CMD		= 0,	//命令接收状态 
-	STATE_TX_TEST	= 1,	//连续发送数据包状态(速度测试)  
-	STATE_RX_TEST	= 2		//连续接收数据包状态(速度测试)  
+	STATE_CMD		= 0,	//receiving command
+	STATE_TX_TEST	= 1,	//continious Tx packet
+	STATE_RX_TEST	= 2		//continious Rx packet  
 };	 
-//定义 uip_tcp_appstate_t 数据类型，用户可以添加应用程序需要用到
-//成员变量。不要更改结构体类型的名字，因为这个类型名会被uip引用。
-//uip.h 中定义的 	struct uip_conn  结构体中引用了 uip_tcp_appstate_t		  
+
+//define uip_tcp_appstate_t variable type
+//user cannot change the structure's name
+//uip.h call structure below	  
 struct tcp_demo_appstate
 {
 	uint8_t state;
@@ -30,11 +31,11 @@ void tcp_server_demo_appcall(void);
 
 //定义应用程序回调函数 
 #ifndef UIP_APPCALL
-#define UIP_APPCALL tcp_demo_appcall //定义回调函数为 tcp_demo_appcall 
-#endif
+#define UIP_APPCALL tcp_demo_appcall  //define the uIP mission entrance function
+
 /////////////////////////////////////TCP SERVER/////////////////////////////////////
-extern uint8_t tcp_server_databuf[];   		//发送数据缓存	 
-extern uint8_t tcp_server_sta;				//服务端状态   
+extern uint8_t tcp_server_databuf[];   		//buffer for Tx data 
+extern uint8_t tcp_server_sta;			    //server status
 //tcp server 函数
 void tcp_server_aborted(void);
 void tcp_server_timedout(void);
@@ -44,8 +45,8 @@ void tcp_server_newdata(void);
 void tcp_server_acked(void);
 void tcp_server_senddata(void);
 /////////////////////////////////////TCP CLIENT/////////////////////////////////////
-extern uint8_t tcp_client_databuf[];   		//发送数据缓存	 
-extern uint8_t tcp_client_sta;				//客户端状态   
+extern uint8_t tcp_client_databuf[];   		//buffer for Tx data  
+extern uint8_t tcp_client_sta;				//client status 
 void tcp_client_reconnect(void);
 void tcp_client_connected(void);
 void tcp_client_aborted(void);
@@ -55,29 +56,4 @@ void tcp_client_acked(void);
 void tcp_client_senddata(void);
 ////////////////////////////////////////////////////////////////////////////////////
 
-
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
