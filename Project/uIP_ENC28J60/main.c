7df19bf0e56bc459e842dd8c5ccfd89f16b322a1
RdMaxes <yc.huang0522@gmail.com>
2014-07-26 03:15:52 +0800

    add polling mision and include uip_arp.h

--- Project/uIP_ENC28J60/main.c
+++ Project/uIP_ENC28J60/main.c
@@ -9,9 +9,10 @@
 #include "uip.h"
 #include "tapdev.h"
 #include "timer.h" //it is timer.h of uIP Lib
+#include "uip_arp.h"
 
 void uip_polling(void);	//prototype of uIP check job
- 												
+
 #define BUF ((struct uip_eth_hdr *)&uip_buf[0])	
 
 static void delay(uint32_t delay_count)
@@ -74,5 +75,82 @@ int main(void)
 			tcp_client_tsta=tcp_client_sta;	//update current server status
 		}
 		delay(8000);		
+	}
+}
+
+
+//uIP event check
+//need to be called eevery cycle of main process
+void uip_polling(void)
+{
+	u8 i;
+	static struct timer periodic_timer, arp_timer;
+	static u8 timer_ok=0;	 
+	if(timer_ok==0)//initialize timer at first time
+	{
+		timer_ok = 1;
+		timer_set(&periodic_timer,CLOCK_SECOND/2); //creat a timer for 0.5 sec 
+		timer_set(&arp_timer,CLOCK_SECOND*10);	   //creat a timer for 10 sec 
+	}				 
+	uip_len=tapdev_read();	//read an IP packet form ehternet.
+							//get data length uip_len which defined in uip.c 
+	if(uip_len>0) 			//if data exist
+	{   
+		//handle IP packet, only the verified packet is used 
+		if(BUF->type == htons(UIP_ETHTYPE_IP))//IP packet? 
+		{
+			uip_arp_ipin();	//delete ethernet part
+			uip_input();   	//IP packet handle
+			//if data transmission is necessary
+			//Tx data is in the uip_buf, length is uip_len	    
+			if(uip_len>0)//response data is necessary
+			{
+				uip_arp_out();
+				tapdev_send();
+			}
+		}else if (BUF->type==htons(UIP_ETHTYPE_ARP)) //is ARP packet?
+		{
+			uip_arp_arpin();
+			//if data transmission is necessary
+			//Tx data is in the uip_buf, length is uip_len	
+ 			if(uip_len>0)tapdev_send(); //send data via tapdev_send()	 
+		}
+	}else if(timer_expired(&periodic_timer))	//if 0.5sec timer expired
+	{
+		timer_reset(&periodic_timer);		//reset 0.5sec timer 
+		//handle every TCP link  
+		for(i=0;i<UIP_CONNS;i++)
+		{
+			uip_periodic(i);	//handle TCP mission  
+			//if data transmission is necessary
+			//Tx data is in the uip_buf, length is uip_len	    
+			if(uip_len>0)//response data is necessary
+			{
+				uip_arp_out();
+				tapdev_send();
+			}
+		}
+
+		#if UIP_UDP	//if uIP_UDP enabled 
+			//handle every UDP link 
+			for(i=0;i<UIP_UDP_CONNS;i++)
+			{
+				uip_udp_periodic(i);	//handle UDP mission 
+				//if data transmission is necessary
+				//Tx data is in the uip_buf, length is uip_len	    
+				if(uip_len>0)//response data is necessary
+				{
+					uip_arp_out();
+					tapdev_send();
+				}
+			}
+		#endif 
+
+		//check if 10sec pass already, for updating ARP 
+		if(timer_expired(&arp_timer))
+		{
+			timer_reset(&arp_timer);
+			uip_arp_timer();
+		}
 	}
 }
