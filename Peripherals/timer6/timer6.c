27702dc00257f6787e6b14365f90584d349747e0
RdMaxes <yc.huang0522@gmail.com>
2014-07-25 14:31:50 +0800

    add timer6 related files

---
+++ Peripherals/timer6/timer6.c
@@ -0,0 +1,40 @@
+#include "timer6.h"
+
+u32 uip_timer=0;//uip system counter
+
+//Timer6 global interrupt handler
+void TIM6_IRQHandler(void)
+{ 	if (TIM_GetITStatus(TIM6, TIM_IT_Update) != RESET) 
+	{
+  		uip_timer++;	
+	} 
+	TIM_ClearITPendingBit(TIM6, TIM_IT_Update  );
+				    		  			    	    
+}
+ 
+//setup Timer6
+//arr: period -1
+//psc: prescaler -1
+void TIM6_Int_Init(u16 arr,u16 psc)
+{	
+    TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE); 
+
+	TIM_TimeBaseStructure.TIM_Period = arr; 
+	TIM_TimeBaseStructure.TIM_Prescaler =psc;   
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0; 
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  
+	TIM_TimeBaseInit(TIM6, &TIM_TimeBaseStructure); 
+ 
+	TIM_ITConfig( TIM6,TIM_IT_Update|TIM_IT_Trigger,ENABLE);
+ 
+	TIM_Cmd(TIM6, ENABLE);  
+ 	
+  	NVIC_InitStructure.NVIC_IRQChannel = TIM6_IRQn;  
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;  
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;  
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; 
+	NVIC_Init(&NVIC_InitStructure);   								 
+}
