e4bedefdcc922b2414b3093027c184d933aea213
RdMaxes <yc.huang0522@gmail.com>
2014-07-25 10:43:32 +0800

    change name, pin and port of LEDs

--- Hardware/LED/led.h
+++ Hardware/LED/led.h
@@ -5,18 +5,18 @@
 #include "stm32f10x_conf.h"
 
 /*LED1 Pin Definition*/
-#define LED1_Port		GPIOB
-#define LED1_CLK		RCC_APB2Periph_GPIOB
-#define LED1_Pin		GPIO_Pin_3
-#define RED_OFF();	    GPIO_ResetBits( LED1_Port, LED1_Pin );  
-#define RED_ON();		GPIO_SetBits( LED1_Port, LED1_Pin );
+#define LED1_Port		GPIOA
+#define LED1_CLK		RCC_APB2Periph_GPIOA
+#define LED1_Pin		GPIO_Pin_1
+#define LED1_ON();		GPIO_ResetBits( LED1_Port, LED1_Pin );  
+#define LED1_OFF();		GPIO_SetBits( LED1_Port, LED1_Pin );
 
 /*LED2 Pin Definition*/
 #define LED2_Port		GPIOA
 #define LED2_CLK		RCC_APB2Periph_GPIOA
-#define LED2_Pin		GPIO_Pin_15
-#define GREEN_OFF();	GPIO_ResetBits( LED2_Port, LED2_Pin );  
-#define GREEN_ON();		GPIO_SetBits( LED2_Port, LED2_Pin );
+#define LED2_Pin		GPIO_Pin_0
+#define LED2_ON();		GPIO_ResetBits( LED2_Port, LED2_Pin );  
+#define LED2_OFF();		GPIO_SetBits( LED2_Port, LED2_Pin );
 /*Function Prototype*/
 //LED Initial 
 void LED_Init(void);
